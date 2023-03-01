56e18c2652b91140b6607fc3e1187d065f367e7e
Nin <darshan310268@gmail.com>
2023-03-01 12:25:04 +0530

    create 'StaffOnly' precondition

---
+++ src/preconditions/StaffOnly.js
@@ -0,0 +1,38 @@
+const { AllFlowsPrecondition } = require('@sapphire/framework');
+const { owners } = require('../config.json');
+
+class UserPrecondition extends AllFlowsPrecondition {
+	#message = 'This command can only be used by the staff.';
+
+	/**
+	 * @param {import('discord.js').CommandInteraction} interaction
+	 */
+	chatInputRun(interaction) {
+		return this.doStaffCheck(interaction.member);
+	}
+
+	/**
+	 * @param {import('discord.js').ContextMenuCommandInteraction} interaction
+	 */
+	contextMenuRun(interaction) {
+		return this.doStaffCheck(interaction.member);
+	}
+
+	/**
+	 * @param {import('discord.js').Message} message
+	 */
+	messageRun(message) {
+		return this.doStaffCheck(message.member);
+	}
+
+	/**
+	 * @param {import('discord.js').GuildMember} member
+	 */
+	doStaffCheck(member) {
+		return member.roles.cache.some((r) => r.name.toLowerCase().includes('staff')) ? this.ok() : this.error({ message: this.#message });
+	}
+}
+
+module.exports = {
+	UserPrecondition
+};
