d267e6a699653c0bdf2acad28d81d61c8d27e6b0
Nin <darshan310268@gmail.com>
2023-03-13 20:44:50 +0530

    better chat

--- src/listeners/guild/chatBot.js
+++ src/listeners/guild/chatBot.js
@@ -1,5 +1,7 @@
 const { Listener } = require('@sapphire/framework');
-const { ChatBot } = require('../../assets/chatbot');
+const { Client } = require('smartestchatbot');
+
+const Chat = new Client(process.env.LEBYY_API);
 
 class ChatEvent extends Listener {
 	constructor(context, options) {
@@ -17,8 +19,42 @@ class ChatEvent extends Listener {
 		if (msg.system) return;
 		if (!msg.guild) return;
 
-		if (msg.channel.name.includes('chatbot')) {
-			await ChatBot(msg);
+		// if (msg.channel.name.includes('chatbot')) {
+		if (msg.channelId === '926319580790550578') {
+			try {
+				if (!msg.content) return;
+				if (msg.content.length < 2) return;
+				msg.channel.sendTyping();
+
+				const res = await Chat.chat(
+					{
+						message: msg.content,
+						name: 'Rajalakshmi',
+						master: 'Darshan',
+						user: msg.author.id,
+						age: '420',
+						birthday: 'September 25',
+						gender: 'female',
+						boyfriend: 'Lorenz',
+						girlfriend: 'Lorenz',
+						nationality: 'Indian',
+						country: 'India',
+						birthplace: 'Bangalore',
+						state: 'Karnataka',
+						location: 'India',
+						email: 'no',
+						website: 'https://darshan-1.gitbook.io/bangalore-hub/',
+						sign: 'Libra',
+						friend: 'Lorenz',
+						mother: 'Vijaylakshmi'
+					},
+					'en'
+				);
+
+				msg.reply({ content: res });
+			} catch (error) {
+				msg.client.logger.error(error);
+			}
 		}
 	}
 }
