e91035913ce3760e3c7a2ee273f2ee931f2ce66b
Nin <darshan310268@gmail.com>
2023-03-25 22:28:04 +0530

    create cron job

--- src/listeners/ready.js
+++ src/listeners/ready.js
@@ -1,7 +1,8 @@
 const { Listener } = require('@sapphire/framework');
 const { blue, gray, green, magenta, magentaBright, white, yellow } = require('colorette');
-const { ActivityType } = require('discord.js');
+const { ActivityType, AttachmentBuilder } = require('discord.js');
 const { joinVoiceChannel, VoiceConnectionStatus } = require('@discordjs/voice');
+const { CronJob } = require('cron');
 
 const dev = process.env.NODE_ENV !== 'production';
 
@@ -70,12 +71,33 @@ class UserEvent extends Listener {
 		setInterval(async () => {
 			const guild = this.container.client.guilds.cache.get(gId);
 			if (parseInt(vCh.name.replace('Members・', '')) === guild.memberCount) return;
-			await vCh.edit({ name: `Members・` }).catch(() => {});
+			await vCh.edit({ name: `Members・${guild.memberCount}` }).catch(() => {});
 			this.container.client.logger.info('Updated Member Count');
 		}, 60 * 1000);
 
 		// ready!
 		this.container.client.logger.debug(`Logged in as ${this.container.client.user.tag} (${this.container.client.id})`);
+
+		// cron job for database backup
+		const job = new CronJob(
+			'0 */12 * * *',
+			async () => {
+				const channel = this.container.client.channels.cache.get('1089230317430247495');
+				const file = new AttachmentBuilder('data/database.sqlite', { name: 'database.sqlite' });
+				await channel.send({ content: `Backup: \`${new Date().toString()}\``, files: [file] }).catch(() => {});
+				this.container.client.logger.debug(`Sent Database Backup [${new Date().toString()}]`);
+			},
+			null,
+			false,
+			'Asia/Kolkata'
+		);
+
+		try {
+			job.start();
+			this.container.client.logger.debug('Cron Job has started!');
+		} catch (error) {
+			this.container.client.logger.error(error);
+		}
 	}
 
 	printBanner() {
