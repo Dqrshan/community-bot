ed1680617122b2afdaaad09ac42dfaec2e80bed8
Lorenz <darshan310268@gmail.com>
2023-07-14 19:32:48 +0530

    update ping.ts

--- src/commands/General/ping.ts
+++ src/commands/General/ping.ts
@@ -1,9 +1,8 @@
-import { ApplicationCommandType, Message } from "discord.js";
+import { Message } from "discord.js";
 import type { Command } from "../../lib/command";
 
 const ping: Command = {
     name: "ping",
-    type: ApplicationCommandType.ChatInput,
     description: "Latency of the bot",
     messageRun: async (msg: Message) => {
         return msg.reply(`🏓 Pong! \`${msg.client.ws.ping}\`ms`);
