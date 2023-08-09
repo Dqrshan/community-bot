9e66322549170b1c8b57d8a28d400b345778a834
Lorenz <darshan310268@gmail.com>
2023-08-09 11:21:30 +0530

    move `splitContent` to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { verifyString } from "discord.js";
 import util from "util";
 
 export const sleep = util.promisify(setTimeout);
@@ -10,4 +11,35 @@ export const chunkify = (arr: any[], len: number) => {
         chunks.push(arr.slice(i, (i += len)));
     }
     return chunks;
+};
+
+export const splitContent = (content: string, limit: number) => {
+    const char = [new RegExp(`.{1,${limit}}`, "g"), "\n"];
+    const text = verifyString(content);
+    if (text.length <= limit) return [text];
+    let splitText = [text];
+
+    while (char.length > 0 && splitText.some((elem) => elem.length > limit)) {
+        const currentChar = char.shift();
+        if (currentChar instanceof RegExp) {
+            splitText = splitText
+                .flatMap((chunk) => chunk.match(currentChar))
+                .filter((value) => value !== null) as string[];
+        } else {
+            splitText = splitText.flatMap((chunk) => chunk.split(currentChar!));
+        }
+    }
+    if (splitText.some((elem) => elem.length > limit)) {
+        throw new RangeError("SPLIT_MAX_LEN");
+    }
+    const messages = [];
+    let msg = "";
+    for (const chunk of splitText) {
+        if (msg && (msg + char + chunk).length > limit) {
+            messages.push(msg);
+            msg = "";
+        }
+        msg += (msg && msg !== "" ? char : "") + chunk;
+    }
+    return messages.concat(msg).filter((m) => m);
 };
